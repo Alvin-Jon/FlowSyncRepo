ad42694679615f7c6d597c84ed234e9153e446f7
Alvin Jon <ingbianjonathan@gmail.com>
2025-11-20 14:03:36 +0100

    hmm

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;  
+const PORT = process.env.PORT || 10000;  
 const http = require('http');
 const server = http.createServer(app);
 
