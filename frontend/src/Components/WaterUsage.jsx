acf19d29ce0361e240280a42280bde006dba86d9
Alvin Jon <ingbianjonathan@gmail.com>
2025-12-03 23:46:03 +0100

    fixed usage bug

--- frontend/src/Components/WaterUsage.jsx
+++ frontend/src/Components/WaterUsage.jsx
@@ -1,7 +1,7 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
 const WaterUsage = (usage) => {
-  const data = [...usage.usage];
+  const data = usage.usage || [];
     return (
     <div className="card" style={{ width: "100%", height: "350px", marginTop: "40px", padding: "20px", paddingBottom: "50px" }}>
       <h3 style={{ textAlign: "left", marginBottom: "1rem" }}>Average Daily Water Usage (L)</h3>
